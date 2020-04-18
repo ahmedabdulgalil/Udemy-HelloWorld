e9875be825eb694b076c4d6fc7abb54575d1b53d
Abdul-Mac <Abdul-Mac@abdulgalils-mbp>
2020-04-18 11:59:50 -0400

    created a variable and print out to console

--- Udemy-HelloWorld/src/Hello.java
+++ Udemy-HelloWorld/src/Hello.java
@@ -4,7 +4,12 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello, Abdul. ");
+		System.out.println("Hello, Abdul. "); // this line print the sentence inside the ()
+		
+		int myFirstNumber = 5; // declared a variable to int and set value to 5. 
+		
+		System.out.println(myFirstNumber); // this line print out the variable from previous line.
+		
 
 	}
 
